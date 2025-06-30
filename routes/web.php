f7ea50fdf40f904db5e55198bc8e1d31da86e23c
mgmhue <mghmue2000@gmail.com>
2025-06-30 22:18:53 +0630

    l1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hi', function () {
+    return 'Hi Laravel';
+});
+
+Route::get('/Hello', function () {
+    return view('hello');
+});
+
+
