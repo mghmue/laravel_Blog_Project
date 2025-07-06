946152a0fae483855a8eeb0056371c5a4842e6d5
mgmhue <mghmue2000@gmail.com>
2025-07-06 20:25:53 +0630

    day5

---
+++ app/Http/Controllers/BackendTemplateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BackendTemplateController extends Controller
+{
+    public function tablefun()
+    {
+        return view('backend.table');
+    }
+}
--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -6,18 +6,30 @@ use Illuminate\Http\Request;
 
 class TemplateController extends Controller
 {
-    public function about()
-    {
+    public function index(){
+        $todos = [
+            [ 
+                "title" => 'Learn Laravel', 
+                "body" => 'Laravel is a web application framework with expressive, elegant syntax. Laravel is a tool to help you create strong, beautiful applications using the PHP programming language.'
+            ],
+            [
+                "title" => 'Learn Vue',
+                "body" => 'Vue is a progressive framework for building user interfaces.'
+            ],
+            [
+                "title" => 'Learn React',
+                "body" => 'React makes it painless to create interactive UIs.'
+            ]
+        ];
+        return view('mytemplate.index', compact('todos'));
+    }
+    public function about(){
         return view('mytemplate.about');
     }
-
-    public function contact()
-    {
-        return view('mytemplate.contact');
-    }
-
-    public function post()
-    {
+    public function post(){
         return view('mytemplate.post');
+    }
+    public function contact(){
+        return view('mytemplate.contact');
     }
 }
